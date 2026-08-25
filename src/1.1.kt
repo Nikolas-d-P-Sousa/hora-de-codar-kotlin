f8ab4193d5e81c54a042fde12c250bb1071fe78d
NIKOLAS DE PAULA SOUSA <nikolas.psousa@senacsp.edu.br>
2026-08-25 11:52:23 -0300

    Initial commit

---
+++ src/1.1.kt
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    val nomeDoCarro = "fusca"
+
+    print("Insira seu nome: ")
+    val nome = readlnOrNull().orEmpty()
+    print("Qual a sua idade ? ")
+    val idade = readlnOrNull().orEmpty()
+    val nomeCar = nome.length
+    println("Olá, ${nome.uppercase()}! Você tem $idade anos e seu nome tem $nomeCar carácteres")
+    println("Há um $nomeDoCarro a sua frente. Em sua traseira está escrito 'There is no Hope :)'")
+
+
+
+}
---
+++ src/2.6.kt
@@ -0,0 +1,16 @@
+fun main(){
+    val nums = arrayOf(0,0,0,0,)
+    for (i in 1..4){
+        print("Insira um número: ")
+        nums[i-1] = readln().toInt()
+
+    }
+    print("O primerio número é ")
+    println(nums[0])
+    print("O último número é ")
+    println(nums[3])
+    nums.sortByDescending { it }
+    print("O maior número é ")
+    println(nums[0])
+
+}
